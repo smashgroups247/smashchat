74e19e09663be197a8690dd4d6c326464365fd94
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-06 21:52:10 +0100

    fix: resolve background image position issues

--- src/component/MarketplaceSpotlightSection.jsx
+++ src/component/MarketplaceSpotlightSection.jsx
@@ -4,7 +4,7 @@ export default function MarketplaceSpotlightSection() {
   return (
     <section className="relative bg-white py-[3rem] sm:py-[4rem] md:py-[5rem] lg:py-[6rem] xl:py-[8rem] overflow-hidden font-[Helvetica,Arial,sans-serif]">
       {/* Background Image */}
-      <div className="absolute top-0 right-0 w-[40rem] h-[40rem] sm:w-[50rem] sm:h-[50rem] lg:w-[63.3125rem] lg:h-[63.3125rem] pointer-events-none opacity-100 z-0 lg:left-[-5.5rem]">
+      <div className="absolute top-0 right-0 w-[40rem] h-[40rem] sm:w-[50rem] sm:h-[50rem] lg:w-[63.3125rem] lg:h-[63.3125rem] pointer-events-none opacity-100 z-0 left-[-20%] max-[424px]:left-[-30%] lg:left-[-5.5rem]">
         <img 
           src="/marketplace-bg.png" 
           alt="" 
