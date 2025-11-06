700d02c6d25d8fdfa797f501b454a10958e8e963
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-06 22:03:51 +0100

    Implement market-place section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./component/HeroSection.jsx";
 import WhySmashChatSection from "./component/WhySmashChatSection.jsx";
+import MarketplaceSpotlightSection from "./component/MarketplaceSpotlightSection.jsx";
 
 const App = () => {
   return (
     <div>
       <HeroSection />
       <WhySmashChatSection />
+      <MarketplaceSpotlightSection />
     </div>
   );
 };
