132450b3c6ecb9b1a7a5c3ca1cdf6d3037d65c3c
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-07 09:52:06 +0100

    Implement middle-right image

--- src/component/CTASection.jsx
+++ src/component/CTASection.jsx
@@ -47,6 +47,24 @@ export default function CTASection() {
                   />
                 </div>
 
+                {/* Middle-center image */}
+                <div className="col-span-1 row-span-2 row-start-3">
+                  <img 
+                    src="/cta-user-5.png" 
+                    alt="SmashChat user" 
+                    className="w-full h-full object-cover rounded-[1rem]"
+                  />
+                </div>
+
+                {/* Middle-right image */}
+                <div className="col-span-1 row-span-1 row-start-3">
+                  <img 
+                    src="/cta-user-6.png" 
+                    alt="SmashChat user" 
+                    className="w-full h-full object-cover rounded-[1rem]"
+                  />
+                </div>
+
               </div>
             </div>
           </div>
