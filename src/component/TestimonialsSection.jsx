713cfbd95671d3c31627227d1f5f27f23c888221
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-07 12:39:12 +0100

    fix: improve testimonial details

--- src/component/TestimonialsSection.jsx
+++ src/component/TestimonialsSection.jsx
@@ -28,7 +28,7 @@ export default function TestimonialsSection() {
     },
     {
       text: "I used to dread shopping. Now, I chat with vendors directly.",
-      name: "Olamide",
+      name: "Olamide Johnson",
       avatar: "/testimonial-avatar-3.png"
     },
     {
