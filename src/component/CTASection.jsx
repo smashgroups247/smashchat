b655497369423f03555cf1c19d03fffd78d75d80
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-10 10:51:27 +0100

    fix: resolve decoration star responsiveness issues on mobile devices

--- src/component/CTASection.jsx
+++ src/component/CTASection.jsx
@@ -100,7 +100,7 @@ export default function CTASection() {
           <StarsDecorationWhite className="hidden lg:block top-[54.0625rem] right-[8.25rem] w-[6.088rem] h-[6.801rem] lg:top-[-6rem] lg:right-[58.25rem] xl:top-[-8rem] xl:right-[84.25rem] 2xl:right-[82.55rem] 2xl:top-[-8rem]" />
 
           {/* Stars Decoration - Mobile */}
-          <StarsDecorationWhite className="top-[28rem] right-[45rem] w-[3rem] h-[3.5rem] max-[424px]:top-[25rem] max-[424px]:right-[20rem] md:right-[44rem] md:top-[28rem] lg:hidden" />
+          <StarsDecorationWhite className="top-[25rem] right-[23rem] w-[3rem] h-[3.5rem] max-[424px]:top-[25rem] max-[424px]:right-[20rem] md:right-[44rem] md:top-[28rem] lg:hidden" />
 
           {/* Left Side - Content - Shows SECOND on mobile */}
           <div className="relative space-y-[1.5rem] sm:space-y-[2rem] lg:space-y-[2rem] order-2 lg:order-1 w-full">
