b81b99a29ba8c6f932c5ff06e400daa0f850e387
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-10 13:28:53 +0100

    Implement page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import React from "react";
-import HeroSection from "./component/HeroSection.jsx";
-import WhySmashChatSection from "./component/WhySmashChatSection.jsx";
-import MarketplaceSpotlightSection from "./component/MarketplaceSpotlightSection.jsx";
-import TestimonialsSection from "./component/TestimonialsSection.jsx";
-import CTASection from "./component/CTASection.jsx";
-import Footer from "./component/Footer.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import HomePage from "./pages/HomePage";
+import AboutUsPage from "./pages/AboutUsPage";
+import FeaturesPage from "./pages/FeaturesPage";
+import HowItWorksPage from "./pages/HowItWorksPage";
+import BecomeAVendorPage from "./pages/BecomeAVendorPage";
+import SupportPage from "./pages/SupportPage";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <HeroSection />
-      <WhySmashChatSection />
-      <MarketplaceSpotlightSection />
-      <TestimonialsSection />
-      <CTASection />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        {/* Layout wrapper for all routes */}
+        <Route path="/" element={<Layout />}>
+          {/* Home page route */}
+          <Route index element={<HomePage />} />
+          
+          {/* Navigation routes */}
+          <Route path="about-us" element={<AboutUsPage />} />
+          <Route path="features" element={<FeaturesPage />} />
+          <Route path="how-it-works" element={<HowItWorksPage />} />
+          <Route path="become-a-vendor" element={<BecomeAVendorPage />} />
+          <Route path="support" element={<SupportPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
