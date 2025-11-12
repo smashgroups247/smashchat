f0b89ab5f0d1af6350231f1814c24fc9ba644ffe
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-12 12:32:07 +0100

    our vision component

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,12 +1,14 @@
 import AboutHeroSection from "../component/AboutHeroSection";
 import OurStorySection from "../component/OurStorySection";
 import OurMissionSection from "../component/OurMissionSection";
+import OurVisionSection from "../component/OurVisionSection";
 const AboutUsPage = () => {
   return (
     <>
       <AboutHeroSection />
       <OurStorySection />
       <OurMissionSection />
+      <OurVisionSection />
     </>
   );
 };
