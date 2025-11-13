f02758eb79466d94ea388bbbb9dbd76a090d5fb4
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-13 10:13:44 +0100

    fix: adjust decorative elements alignment

--- src/component/SignupSection.jsx
+++ src/component/SignupSection.jsx
@@ -6,16 +6,18 @@ export default function SignupSection() {
     <section className="relative bg-white py-[3rem] sm:py-[4rem] md:py-[5rem] lg:py-[6rem] xl:py-[8rem] overflow-hidden font-[Helvetica,Arial,sans-serif]">
       {/* Blue Stars Decoration - Top Right */}
       <StarsDecoration className="hidden lg:block absolute top-[1rem] right-[1rem] w-[6rem] h-[6rem] z-10 lg:top-[8rem] xl:top-[8rem] 2xl:top-[8rem]" />
-      {/* Blue Stars Decoration - Mobile */}
-      <StarsDecoration className="block lg:hidden absolute top-[10rem] right-[0rem] w-[2rem] h-[2rem] z-10" />
+      {/* Blue Stars Decoration - Mobile Right */}
+      <StarsDecoration className="block lg:hidden absolute top-[10rem] right-[0rem] w-[4rem] h-[4rem] z-10 max-[424px]:w-[3rem] max-[424px]:h-[3rem] max-[424px]:top-[11rem]" />
+        {/* Blue Stars Decoration - Mobile Left */}
+      <StarsDecoration className="block lg:hidden absolute top-[28.125rem] left-[-0.625rem] max-[424px]:w-[3rem] max-[424px]:h-[3rem] md:top-[42rem] md:right-[44rem] w-[4rem] h-[4rem] z-10" />
       <div className="max-w-[90rem] mx-auto px-[1rem] sm:px-[1.5rem] md:px-[3rem] lg:px-[5rem] xl:px-[6rem] relative z-10">
         {/* Content Grid */}
         <div className="flex flex-col lg:grid lg:grid-cols-2 gap-[2rem] lg:gap-[3rem] xl:gap-[4rem] items-center">
           {/* Left Side - Phone Mockups - Shows SECOND on mobile */}
-          <div className="relative w-full flex justify-center lg:justify-start order-2 lg:order-1">
+    <div className="relative w-full flex justify-center lg:justify-start order-2 lg:order-1">
             <div className="relative w-full max-w-[20rem] sm:max-w-[25rem] md:max-w-[30rem] lg:max-w-[34.0625rem] h-[25rem] sm:h-[30rem] md:h-[35rem] lg:h-[44.1875rem]">
               {/* Bottom-left phone (behind) */}
-              <div className="absolute z-10 w-[60%] sm:w-[65%] lg:w-[17.875rem] top-[1.5rem] sm:top-[2rem] lg:top-[1.375rem] left-0">
+              <div className="absolute z-10 w-[60%] sm:w-[65%] lg:w-[17.875rem] top-[1.5rem] sm:top-[2rem] lg:top-[1.375rem] left-0 max-[424px]:left-[0.625rem]">
                 <img
                   src="/signup-phone-left.png"
                   alt="SmashChat interface"
@@ -24,7 +26,7 @@ export default function SignupSection() {
               </div>
 
               {/* Top-right phone (front) */}
-              <div className="absolute z-20 w-[70%] sm:w-[75%] lg:w-[19.6875rem] top-0 right-0 sm:right-[0.5rem] lg:right-0">
+              <div className="absolute z-20 w-[70%] sm:w-[75%] lg:w-[19.6875rem] top-0 right-[-1.5rem] sm:right-[0.5rem] md:right-[-5.5rem] lg:right-0 max-[424px]:right-[-0.5rem]">
                 <img
                   src="/signup-phone-right.png"
                   alt="Sign up interface"
@@ -32,7 +34,7 @@ export default function SignupSection() {
                 />
               </div>
             </div>
-          </div>
+          </div>      
 
           {/* Right Side - Content - Shows FIRST on mobile */}
           <div className="relative space-y-[1.5rem] sm:space-y-[2rem] lg:space-y-[2rem] order-1 lg:order-2 w-full">
