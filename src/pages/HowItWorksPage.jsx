eada2abbfbf78daa0038a5307807e3dc4899c6f6
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-13 09:19:08 +0100

    Implement sign-up section

--- src/pages/HowItWorksPage.jsx
+++ src/pages/HowItWorksPage.jsx
@@ -1,9 +1,11 @@
 import HowItWorksHero from "../component/HowItWorksHero";
+import SignupSection from "../component/SignupSection";
 
 const HowItWorksPage = () => {
   return (
     <>
     <HowItWorksHero />
+    <SignupSection />
     </>
   );
 };
