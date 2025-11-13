4a038a4483c20f608eff1b9117d28b1cb31587bb
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-13 10:54:13 +0100

    Implement sync contact section

--- src/pages/HowItWorksPage.jsx
+++ src/pages/HowItWorksPage.jsx
@@ -1,11 +1,13 @@
 import HowItWorksHero from "../component/HowItWorksHero";
 import SignupSection from "../component/SignupSection";
+import SyncContactsSection from "../component/SyncContactsSection";
 
 const HowItWorksPage = () => {
   return (
     <>
     <HowItWorksHero />
     <SignupSection />
+    <SyncContactsSection />
     </>
   );
 };
