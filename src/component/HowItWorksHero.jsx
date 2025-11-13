762df05bfe89fbc652d07da20c7f4c0c116c1c60
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2025-11-13 08:16:12 +0100

    fix: adjust decorative elements position

--- src/component/HowItWorksHero.jsx
+++ src/component/HowItWorksHero.jsx
@@ -1,14 +1,14 @@
-
 import StarsDecoration from './StarsDecoration';
 import DownloadAppButton from './DownloadAppButton';
+
 export default function HowItWorksHero() {
   return (
-        <section className="relative bg-white pt-[8rem] lg:pt-[18rem] pb-[3rem] sm:pb-[4rem] md:pb-[5rem] lg:pb-[6rem] xl:pb-[8rem] overflow-hidden font-[Helvetica,Arial,sans-serif]">
+    <section className="relative bg-white pt-[8rem] lg:pt-[18rem] pb-[3rem] sm:pb-[4rem] md:pb-[5rem] lg:pb-[6rem] xl:pb-[8rem] overflow-hidden font-[Helvetica,Arial,sans-serif]">
       {/* Blue Stars Decoration - Top Right */}
-      <StarsDecoration className="hidden lg:block absolute top-[1rem] right-[1rem] w-[3rem] h-[3rem] z-10" />
+      <StarsDecoration className="hidden lg:block absolute top-[1rem] right-[1rem] w-[6rem] h-[6rem] z-10 lg:top-[8rem] xl:top-[8rem] 2xl:top-[8rem]" />
       
       {/* Blue Stars Decoration - Left Side (Desktop) */}
-      <StarsDecoration className="hidden lg:block absolute top-[14rem] left-[2rem] w-[3rem] h-[3rem] z-10" />
+      <StarsDecoration className="hidden lg:block absolute top-[14rem] left-[2rem] w-[6rem] h-[6rem] z-10 lg:top-[22rem] lg:left-[-2rem] xl:top-[21rem] xl:left-[-2rem] 2xl:left-[30rem] " />
       
       {/* Blue Stars Decoration - Mobile */}
       <StarsDecoration className="block lg:hidden absolute top-[1rem] right-[1rem] w-[2rem] h-[2rem] z-10" />
@@ -17,26 +17,14 @@ export default function HowItWorksHero() {
       <img 
         src="/left-decorative-curve.png" 
         alt="decorative curve" 
-        className="hidden lg:block absolute pointer-events-none z-0"
-        style={{
-          width: '4.875rem',
-          height: '4.875rem',
-          top: '27.375rem',
-          left: '1.625rem'
-        }}
+        className="hidden lg:block absolute pointer-events-none z-0 w-[4.875rem] h-[4.875rem] top-[35rem] left-[1.625rem] xl:top-[40rem] 2xl:left-[32rem]"
       />
       
       {/* Right Decorative Curve */}
       <img 
         src="/right-decorative-curve.png" 
         alt="decorative curve" 
-        className="hidden lg:block absolute pointer-events-none z-0"
-        style={{
-          width: '11.25rem',
-          height: '2.1875rem',
-          top: '23.125rem',
-          left: '66.5625rem'
-        }}
+        className="lg:block absolute pointer-events-none z-0 w-[6rem] h-[1rem] lg:w-[11.25rem] lg:h-[2.1875rem] top-[17.5rem] left-[20rem] lg:top-[35rem] lg:left-[46rem] xl:top-[35rem] xl:left-[60rem] 2xl:left-[100rem]"
       />
       
       <div className="max-w-[90rem] mx-auto px-[1rem] sm:px-[1.5rem] md:px-[3rem] lg:px-[5rem] xl:px-[6rem] relative z-10">
